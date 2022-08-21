c74a7d4772162c29e24537a9b2c600e9e0545fa9
itaykarat <itaykarat13@gmail.com>
2022-08-21 18:28:20 +0300

    first commit

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,43 +1,63 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-import torch.nn as nn
-import torch.nn.functional as F
-# import torchvision
-# import torchvision.transforms as transforms
-import sys
-import torch.nn as nn
 
 
-def logistic(Z):
-    return 1 / (1+torch.exp( -Z ))
+""""
+Motivation:
+
+   * The sigmoid function is used as an activation function in neural networks
+    
+    
+Properties of the sigmoid function:
+
+   * Domain: (-∞, +∞)
+   * Range: (0, +1)
+   * σ(0) = 0.5
+   * The function is monotonically increasing.
+   * The function is continuous everywhere.
+   * The function is differentiable everywhere in its domain.
+
+"""
 
 
-def create_data():
-    x = np.arange(-8,8,0.01)
-    print('those are the new data points: ',x)
-    return x
+
+
+# ---------------------------- HELPERS ----------------------------
 
 def convert_numpy_array_to_pytorch(x):
     x = torch.from_numpy(x)
     return x
 
 
-def calculate_y_yat(x):
-    y_hat = logistic(x)
-    return y_hat
+def create_data():
+    x = np.arange(-8, 8, 0.01)
+    x = convert_numpy_array_to_pytorch(x)
+    return x
 
 
-def plot_graph(x,y_hat):
-    plt.title("Sigmoid activation function")
-    plt.plot(x,y_hat)
-    plt.show()
+# ---------------------------- sigmoid class ----------------------------
+
+
+class sigmoid:
+    def __init__(self, data):
+        self.data = data
+
+    def logistic(self, Z):
+        return 1 / (1 + torch.exp(-Z))
+
+    def calculate_y_yat(self, X):
+        y_hat = self.logistic(X)
+        return y_hat
+
+    def plot_graph(self, x, y_hat):
+        plt.title("Sigmoid activation function")
+        plt.plot(self, x, y_hat)
+        plt.show()
 
 
 if __name__ == '__main__':
     data = create_data()
-    data = convert_numpy_array_to_pytorch(data)
-    y_hat = calculate_y_yat(data)
-    plot_graph(data,y_hat)
-
-
+    sigmoid_obj = sigmoid(data=data)
+    y_hat = sigmoid_obj.calculate_y_yat(data)
+    sigmoid_obj.plot_graph(data, y_hat)
