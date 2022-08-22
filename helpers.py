c2756d176f90ccdfe3338e8efc9206de04f6c3c1
itaykarat <itaykarat13@gmail.com>
2022-08-22 17:13:32 +0300

    Added cross entropy implementation
    In order to measure the performance of a model we create some loss function.
    One kind of loss function is cross entropy.

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
-import torch # pytorch API useful for deep learning implementations
-import numpy as np # numpy API for arithmetics
+import matplotlib.pyplot as plt
+import torch  # pytorch API useful for deep learning implementations
+import numpy as np  # numpy API for arithmetics
 
 
 def convert_numpy_array_to_pytorch(X):
@@ -11,3 +12,25 @@ def create_data():
     X = np.arange(-8, 8, 0.01)
     X = convert_numpy_array_to_pytorch(X)
     return X
+
+
+def get_loss_y_0(y_hat):
+    return -np.log(1 - y_hat)
+
+
+def get_loss_y_1(y_hat):
+    return -np.log(y_hat)
+
+
+def get_loss_gen(y, y_hat):
+    return (-y) * np.log(y_hat) - (1 - y) * np.log(1 - y_hat)
+
+
+def plot_losses(y_hat, loss_y_0, loss_y_1):
+    plt.plot(y_hat, loss_y_0, label='$\ell(y=0,\hat{y})$')
+    plt.plot(y_hat, loss_y_1, label='$\ell(y=1,\hat{y})$')
+
+    plt.xlabel('$\hat{y}$')
+    plt.legend()
+    plt.title('cross entropy')
+    plt.show()
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import helpers
 from logistic_regression import sigmoid
 
-
 if __name__ == '__main__':
-    data = helpers.create_data() # generate synthetic datasets
-    sigmoid_obj = sigmoid(Data_points=data) # creating a sigmoid object
+    data = helpers.create_data()  # generate synthetic datasets
+    sigmoid_obj = sigmoid(Data_points=data)  # creating a sigmoid object
 
+    y_hat = sigmoid_obj.calculate_y_yat()  # map each Xi into [0,1]
+    sigmoid_obj.plot_graph(y_hat)  # plot the output with (X,y_hat) for each (Xi,y_hat_i)
 
-    y_hat = sigmoid_obj.calculate_y_yat() # map each Xi into [0,1]
-    sigmoid_obj.plot_graph( y_hat) # plot the output with (X,y_hat) for each (Xi,y_hat_i)
+    # loss_0 = helpers.get_loss_y_0(y_hat)
+    # loss_1 = helpers.get_loss_y_1(y_hat)
+
+    loss_0 = helpers.get_loss_gen(y=0,y_hat=y_hat)
+    loss_1 = helpers.get_loss_gen(y=1,y_hat=y_hat)
+
+    helpers.plot_losses(y_hat=y_hat, loss_y_0=loss_0, loss_y_1=loss_1)
